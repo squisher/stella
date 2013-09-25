633a379699be8c44666e4262915f15175b8b3847
David Mohr <david@mcbf.net>
2013-09-25 10:11:56 -0600

    Better tests

---
+++ stella/setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+setup(
+    name = "stella",
+    version = "0.1",
+    packages = find_packages(),
+)
--- test/addition.py
+++ test/addition.py
@@ -6,6 +6,6 @@ from random import randint
 def addition(a,b):
     return a+b
 
-def addition_test():
+def test_addition():
     for (a,b) in [(0,0), (randint(0, 1000000), randint(0, 1000000))]:
         assert addition(a,b) == stella(addition)(a,b)
