2b9a6084ebeaeaa9ae59b9127a0d0a62773fdb15
David Mohr <david@mcbf.net>
2013-10-14 09:23:22 -0600

    Adding test program for C code

---
+++ Makefile
@@ -0,0 +1,5 @@
+CFLAGS:=-Wall
+CC=gcc
+
+ctest: ctest.o
+ctest.o: ctest.c
---
+++ ctest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char ** argv) {
+    printf ("floor(2.5) = %f\n", floor(2.5));
+    printf ("5 / -2 = %d\n", 5 / -2);
+    printf ("5.0 / -2 = %f\n", 5.0 / -2);
+    printf ("5 / -2.0 = %f\n", 5 / -2.0);
+    printf ("\n");
+
+    /* test case chained() */
+    int a = 341433;
+    int b = 673069;
+    printf ("a=%d, b=%dfor (a-b)/b*a;\n", a, b);
+    float rf = ((a-b)/(float)b)*a;
+    double rd = ((a-b)/(double)b)*a;
+    printf ("Python =%10.10f\nStella =%10.10f\n", -168231.59941699886, -168231.609375);
+    printf ("Cfloat =%10.10f\nCdouble=%10.10lf\n", rf, rd);
+    return 0;
+}
