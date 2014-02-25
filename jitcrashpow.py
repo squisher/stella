160281602b394e938039396a1c9785e7892819f8
David Mohr <david@mcbf.net>
2014-02-25 14:12:11 -0700

    Eliminating type trunc from standalone example

--- jitcrashpow.py
+++ jitcrashpow.py
@@ -6,10 +6,9 @@ from llvm.ee import *
 def power_jit(arg1_value, arg2_value):
     my_module = Module.new('my_module')
 
-    ty_int = Type.int(64)
     ty_double = Type.double()
-    ty_func = Type.function(ty_double, [ty_double, ty_int])
     ty_int32 = Type.int(32)
+    ty_func = Type.function(ty_double, [ty_double, ty_int32])
 
     f_power = my_module.add_function(ty_func, "power")
     f_power.args[0].name = "a"
@@ -17,9 +16,8 @@ def power_jit(arg1_value, arg2_value):
 
     bb = f_power.append_basic_block("entry")
     builder = Builder.new(bb)
-    tmp = builder.trunc(f_power.args[1], ty_int32, '(i32)b')
     llvm_pow = Function.intrinsic(my_module, INTR_POWI, [ty_double])
-    pow_result = builder.call(llvm_pow, [f_power.args[0], tmp])
+    pow_result = builder.call(llvm_pow, [f_power.args[0], f_power.args[1]])
     builder.ret(pow_result)
 
     print ('# IR ------------------------------------------------------------')
@@ -37,7 +35,7 @@ def power_jit(arg1_value, arg2_value):
     ee = eb.create()
 
     arg1 = GenericValue.real(ty_double, arg1_value)
-    arg2 = GenericValue.int(ty_int, arg2_value)
+    arg2 = GenericValue.int(ty_int32, arg2_value)
 
     retval = ee.run_function(f_power, [arg1, arg2])
 
