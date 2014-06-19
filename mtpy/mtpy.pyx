099d4228fa970fd4062b0121fe226066b6946358
David Mohr <david@mcbf.net>
2014-06-19 15:20:14 -0600

    Use identical names for C and python

--- mtpy/mtpy.pyx
+++ mtpy/mtpy.pyx
@@ -1,15 +1,22 @@
 from math import log
 
+# cdef extern void c_eject_tomato "eject_tomato" (float speed)
 cdef extern from "mtwist-1.1/mtwist.c":
-    double mt_drand()
-    void mt_seed32new(unsigned int)
+    double c_mt_drand "mt_drand" ()
+    void c_mt_seed32new "mt_seed32new" (unsigned int)
 
-def uniform():
-    return mt_drand()
+#def uniform():
+#    return mt_drand()
+#
+#def exp(p):
+#    u = 1.0 - uniform()
+#    return -log(u)/p
+#
+#def seed(s):
+#    mt_seed32new (s)
 
-def exp(p):
-    u = 1.0 - uniform()
-    return -log(u)/p
+def mt_drand():
+    return c_mt_drand()
 
-def seed(s):
-    mt_seed32new (s)
+def mt_seed32new(s):
+    c_mt_seed32new (s)
