26a879c31595ddff4605a89ba4a04be6b1cd259d
David Mohr <david@mcbf.net>
2014-07-22 13:31:54 +0200

    Passing data from llvm to python based on pointers, cffi experiment

--- hell.py
+++ hell.py
@@ -5,10 +5,15 @@ from llvm.core import *
 from llvm.ee import *
 
 import logging
+import ctypes
+import cffi
 
 def playground():
     my_module = Module.new('my_module')
-    tp_int = Type.int(64)   # By default 32bit, indices MUST be 64bit
+    tp_int = Type.int(64)
+    tp_idx = Type.int()
+    tp_struct = Type.struct([tp_int, tp_int, tp_int, tp_int], name='test_struct')
+    #tp_func = Type.function(Type.pointer(tp_struct), [])
     tp_func = Type.function(tp_int, [])
     f_sum = my_module.add_function(tp_func, "sum")
     bb = f_sum.append_basic_block("entry")
@@ -18,30 +23,58 @@ def playground():
                       "tmp")
 
 
-    # Q: How to access members of a freshly stack allocated struct?
-    tp_struct = Type.struct([tp_int, tp_int], name='test_struct')
-    struct = builder.alloca(tp_struct)
-    print (str(struct) + '; ' + repr(struct))
-    p = builder.gep(struct, [Constant.int(tp_int, 0), Constant.int(tp_int, 0)])
+    #struct = builder.alloca(tp_struct)
+    struct = builder.malloc(tp_struct)
+    p = builder.gep(struct, [Constant.int(tp_idx, 0), Constant.int(tp_idx, 0)])
     builder.store(tmp, p)
-    tmp2 = builder.load(p)
+    p = builder.gep(struct, [Constant.int(tp_idx, 0), Constant.int(tp_idx, 1)])
+    builder.store(Constant.int(tp_int, 43), p)
+    p = builder.gep(struct, [Constant.int(tp_idx, 0), Constant.int(tp_idx, 2)])
+    builder.store(Constant.int(tp_int, 44), p)
+    p = builder.gep(struct, [Constant.int(tp_idx, 0), Constant.int(tp_idx, 3)])
+    builder.store(Constant.int(tp_int, 45), p)
+    tmp2 = builder.ptrtoint(struct, tp_int)
 
+    #p = builder.gep(struct, [Constant.int(tp_idx, 0), Constant.int(tp_idx, 0)])
+    #tmp3 = builder.load(p)
+
+    #builder.ret(struct)
+    builder.ret(tmp2)
+    #builder.ret(tmp3)
 
     print(str(my_module))
-    builder.ret(tmp2)
-
-    # Create an execution engine object. This will create a JIT compiler
-    # on platforms that support it, or an interpreter otherwise.
     eb = EngineBuilder.new(my_module)
     eb.mcjit(True)
     ee = eb.create()
 
-    # Now let's compile and run!
     retval = ee.run_function(f_sum, [])
 
-    # The return value is also GenericValue. Let's print it.
-    #logging.debug("returned %d", retval.as_int())
+    #return retval.as_pointer()
     return retval.as_int()
 
+class Point(ctypes.Structure):
+    _fields_ = [
+        ('x', ctypes.c_int64),
+        ('y', ctypes.c_int64),
+        ('z', ctypes.c_int64),
+        ('r', ctypes.c_int64),
+    ]
+
 if __name__ == '__main__':
-    print(playground())
+    p = playground()
+    print("Returned: " + str(p))
+    #cast_p = ctypes.cast(p, ctypes.POINTER(ctypes.c_long))
+
+    ffi = cffi.FFI()
+    ffi.cdef("""typedef struct {
+        int64_t x;
+        int64_t y;
+        int64_t z;
+        int64_t r;
+    } sFoo;""")
+    x = ffi.cast("sFoo *", p)
+    print("cffi:", x, x.x, x.y, x.z, x.r)
+
+    cast_p = ctypes.cast(p, ctypes.POINTER(Point))
+    #import pdb; pdb.set_trace()
+    print("ctypes:", cast_p[0], cast_p[0].x, cast_p[0].y, cast_p[0].z, cast_p[0].r)
