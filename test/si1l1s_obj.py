487d7dee5f35c613179273a8a568b215168c2ad2
David Mohr <david@mcbf.net>
2014-11-26 14:22:39 -0700

    Adding full-fledged si1l1s with minor changes (mtpy)

---
+++ test/si1l1s_obj.py
@@ -0,0 +1,172 @@
+"""
+Semi-infinite 1D strip with a single spider.
+"""
+
+import mtpy  # cython wrapper around mtwist
+from math import log, exp
+import time
+from numpy import zeros
+
+from test import *  # noqa
+import stella
+
+
+class BaseSettings(object):
+
+    def setDefaults(self):
+        self.settings = {
+            'seed': [int(time.time()), int],
+            'r': [0.1, float],
+            'koff': [1.0, float],
+            'radius': [10, int],
+            'nlegs': [2, int],
+            'gait': [2, int],
+            'dim': [2, int],
+            'nspiders': [1, int],     # not completely functional yet
+            'elapsedTime': [self.elapsedTime, lambda x:x],
+        }
+
+    def elapsedTime(self):
+        return time.time() - self.start_time
+
+    def __init__(self, argv=[]):
+        self.start_time = time.time()
+
+        self.setDefaults()
+
+        # parse command line arguments to overwrite the defaults
+        for key, _, val in [s.partition('=') for s in argv]:
+            self[key] = val
+
+    def __setitem__(self, k, v):
+        if k in self.settings:
+            self.settings[k][0] = self.settings[k][1](v)
+        else:
+            self.settings[k] = [v, type(v)]
+
+    def __getitem__(self, k):
+        return self.settings[k][0]
+
+    def __str__(self):
+        r = '{'
+        for k, (v, type_) in self.settings.items():
+            # if isinstance(type_,  FunctionType):
+            #    continue
+            r += str(k) + ':' + str(v) + ', '
+        return r[:-2] + '}'
+
+
+class Settings(BaseSettings):
+
+    def setDefaults(self):
+        self.settings = {
+            'seed': [int(time.time()), int],
+            'r': [0.1, float],
+            'koffp': [1.0, float],
+            'K': [10, int],
+            'rununtiltime': [1e3, float],
+            'elapsedTime': [self.elapsedTime, lambda x:x],
+        }
+
+
+def mtpy_exp(p):
+    u = 1.0 - mtpy.mt_drand()
+    return -log(u) / p
+
+
+class Simulation(object):
+    EXPSTART = 0.2
+
+    def __init__(self, params):
+        self.K = params['K']
+        self.rununtiltime = params['rununtiltime']
+        mtpy.mt_seed32new(params['seed'])
+        self.koffp = params['koffp']
+        self.kcat = params['r']
+
+        self.delta = (
+            log(self.rununtiltime) - log(self.EXPSTART)) / float(self.K - 1)
+        self.leg = 0
+        self.substrate = 0
+        self.obs_i = 0
+        self.observations = zeros(shape=self.K, dtype=int)
+
+    def __eq__(self, o):
+        assert isinstance(o, self.__class__)
+        return (self.observations == o.observations).all()
+
+    def makeObservation(self):
+        """Called from run()"""
+        self.observations[self.obs_i] = self.leg
+        self.obs_i += 1
+
+        self.next_obs_time = self.getNextObsTime()
+
+    def getNextObsTime(self):
+        """Called from run()"""
+        if self.obs_i == 0:
+            return self.EXPSTART
+        if self.obs_i == self.K - 1:
+            return self.rununtiltime
+
+        return exp(log(self.EXPSTART) + self.delta * self.obs_i)
+
+    def step(self):
+        """Called from run()"""
+        if self.leg == 0:
+            self.leg += 1
+        else:
+            u1 = mtpy.mt_drand()
+            if u1 < 0.5:
+                self.leg -= 1
+            else:
+                self.leg += 1
+        if self.leg == self.substrate:
+            self.substrate += 1
+
+    def isNextObservation(self):
+        return self.t > self.next_obs_time and self.obs_i < self.K
+
+    def run(self):
+        self.t = 0.0
+        self.next_obs_time = self.getNextObsTime()
+
+        while self.obs_i < self.K and self.t < self.rununtiltime:
+            if self.leg < self.substrate:
+                R = self.koffp
+            else:
+                R = self.kcat
+            self.t += mtpy_exp(R)
+
+            while self.isNextObservation():
+                self.makeObservation()
+
+            self.step()
+        return self.observations
+
+
+def prototype(params):
+    s = Settings(params)
+
+    sim_py = Simulation(s)
+    sim_py.run()
+
+    sim_st = Simulation(s)
+    stella.wrap(sim_st.run)()
+
+    assert id(sim_py.observations) != id(sim_st.observations)
+    assert sim_py == sim_st
+
+
+@mark.parametrize('args', [['seed=42'], ['seed=63'], ['seed=123456'],
+                           ['rununtiltime=1e4', 'seed=494727']])
+@unimplemented
+def test1(args):
+    prototype(args)
+
+
+timed = timeit(prototype)
+
+
+def bench1():
+    timed(['seed=42', 'rununtiltime=1e8'])
--- wip.py
+++ wip.py
@@ -4,6 +4,7 @@ import test.langconstr
 import test.objects
 import test.external_func
 import test.si1l1s_struct
+import test.si1l1s_obj
 import stella
 from stella import exc  # noqa
 import numpy as np
@@ -17,9 +18,10 @@ b2 = test.objects.B(0.0, 1.0)
 c = test.objects.C(np.array([1, 2, 3, 42]))
 e = test.objects.E()
 e2 = test.objects.E()
-settings = test.si1l1s_struct.Settings(['seed=42'])
+settings = test.si1l1s_obj.Settings(['seed=42'])
+sim = test.si1l1s_obj.Simulation(settings)
 sp = test.si1l1s_struct.Spider(settings, np.zeros(shape=settings['K'], dtype=int))
 
 
 def current_work(run=False):
-    print(stella.wrap(test.objects.callBoundMethodOnTwo, ir=not run)(e, e2))
+    print(stella.wrap(sim.run, ir=not run)())
