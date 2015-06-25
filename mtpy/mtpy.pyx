f2ee1d915ca644da9dfe3d135515c690a36ac51b
David Mohr <david@mcbf.net>
2015-06-25 13:50:15 -0600

    Clean up mtpy

--- mtpy/mtpy.pyx
+++ mtpy/mtpy.pyx
@@ -14,26 +14,13 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#from math import log
-import ctypes
-
-# cdef extern void c_eject_tomato "eject_tomato" (float speed)
 cdef extern from "mtwist-1.1/mtwist.c":
     double c_mt_drand "mt_drand" ()
     void c_mt_seed32new "mt_seed32new" (unsigned int)
 
-#def uniform():
-#    return mt_drand()
-#
-#def exp(p):
-#    u = 1.0 - uniform()
-#    return -log(u)/p
-#
-#def seed(s):
-#    mt_seed32new (s)
-
 def getCSignatures():
     """There should be a way to retrieve this info from cython, but I couldn't find it"""
+    import ctypes
     return {
         'mt_drand': (ctypes.c_double, []),
         'mt_seed32new': (None, [ctypes.c_uint32])
