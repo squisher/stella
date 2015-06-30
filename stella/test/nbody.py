85ed4a6ddac69cc48993346c072c684b95c8e7ff
David Mohr <david@mcbf.net>
2015-06-29 23:33:54 -0600

    Revert nbody change: no reason to avoid dictionary
    The dictionary was never used in the actual simulation core.

--- stella/test/nbody.py
+++ stella/test/nbody.py
@@ -53,6 +53,7 @@ try:
 except SystemError:
     def unimplemented(f):
         return f
+
     def parametrize(*args):
         return unimplemented
 
@@ -89,44 +90,43 @@ class Body(object):
                 raise Exception('{}: {} - {} = {} > {}'.format(a, me, it, me - it, DELTA))
 
 
-class BODIES(object):
-    sun = Body([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS)
+BODIES = {
+    'sun': Body([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
 
-    jupiter = Body([4.84143144246472090e+00,
-                    -1.16032004402742839e+00,
-                    -1.03622044471123109e-01],
-                   [1.66007664274403694e-03 * DAYS_PER_YEAR,
-                    7.69901118419740425e-03 * DAYS_PER_YEAR,
-                    -6.90460016972063023e-05 * DAYS_PER_YEAR],
-                   9.54791938424326609e-04 * SOLAR_MASS)
+    'jupiter': Body([4.84143144246472090e+00,
+                     -1.16032004402742839e+00,
+                     -1.03622044471123109e-01],
+                    [1.66007664274403694e-03 * DAYS_PER_YEAR,
+                     7.69901118419740425e-03 * DAYS_PER_YEAR,
+                     -6.90460016972063023e-05 * DAYS_PER_YEAR],
+                    9.54791938424326609e-04 * SOLAR_MASS),
 
-    saturn = Body([8.34336671824457987e+00,
-                   4.12479856412430479e+00,
-                   -4.03523417114321381e-01],
-                  [-2.76742510726862411e-03 * DAYS_PER_YEAR,
-                   4.99852801234917238e-03 * DAYS_PER_YEAR,
-                   2.30417297573763929e-05 * DAYS_PER_YEAR],
-                  2.85885980666130812e-04 * SOLAR_MASS)
+    'saturn': Body([8.34336671824457987e+00,
+                    4.12479856412430479e+00,
+                    -4.03523417114321381e-01],
+                   [-2.76742510726862411e-03 * DAYS_PER_YEAR,
+                    4.99852801234917238e-03 * DAYS_PER_YEAR,
+                    2.30417297573763929e-05 * DAYS_PER_YEAR],
+                   2.85885980666130812e-04 * SOLAR_MASS),
 
-    uranus = Body([1.28943695621391310e+01,
-                   -1.51111514016986312e+01,
-                   -2.23307578892655734e-01],
-                  [2.96460137564761618e-03 * DAYS_PER_YEAR,
-                   2.37847173959480950e-03 * DAYS_PER_YEAR,
-                   -2.96589568540237556e-05 * DAYS_PER_YEAR],
-                  4.36624404335156298e-05 * SOLAR_MASS)
+    'uranus': Body([1.28943695621391310e+01,
+                    -1.51111514016986312e+01,
+                    -2.23307578892655734e-01],
+                   [2.96460137564761618e-03 * DAYS_PER_YEAR,
+                    2.37847173959480950e-03 * DAYS_PER_YEAR,
+                    -2.96589568540237556e-05 * DAYS_PER_YEAR],
+                   4.36624404335156298e-05 * SOLAR_MASS),
 
-    neptune = Body([1.53796971148509165e+01,
-                    -2.59193146099879641e+01,
-                    1.79258772950371181e-01],
-                   [2.68067772490389322e-03 * DAYS_PER_YEAR,
-                    1.62824170038242295e-03 * DAYS_PER_YEAR,
-                    -9.51592254519715870e-05 * DAYS_PER_YEAR],
-                   5.15138902046611451e-05 * SOLAR_MASS)
+    'neptune': Body([1.53796971148509165e+01,
+                     -2.59193146099879641e+01,
+                     1.79258772950371181e-01],
+                    [2.68067772490389322e-03 * DAYS_PER_YEAR,
+                     1.62824170038242295e-03 * DAYS_PER_YEAR,
+                     -9.51592254519715870e-05 * DAYS_PER_YEAR],
+                    5.15138902046611451e-05 * SOLAR_MASS),
+}
 
-
-SYSTEM = [BODIES.sun, BODIES.jupiter, BODIES.saturn, BODIES.uranus,
-          BODIES.neptune]
+SYSTEM = list(BODIES.values())
 
 
 def advance(dt, n, bodies):
@@ -247,7 +247,7 @@ def test1b(opt):
 def _test1(opt):
     import stella
     n = 5990
-    offset_momentum(BODIES.sun, SYSTEM)
+    offset_momentum(BODIES['sun'], SYSTEM)
     s1 = copy.deepcopy(SYSTEM)
     s2 = copy.deepcopy(SYSTEM)
     r1 = advance(0.01, n, s1)
